b11f077e68a50e529e90adfac92453560357c6d2
CarmelLR <karmelalukasrusnov@gmail.com>
2025-03-18 00:00:07 +0100

    Add files via upload
    za sve te gradove ima jos dodat oko cjene ....neman bas ideju kako da to mislila san još jedan duži card ispod recenzije i onda tu al nez...pomalo :)

--- EDIT-ZP-2024-2025/main.js
+++ EDIT-ZP-2024-2025/main.js
@@ -1,6 +1,13 @@
 const gradovi = [
     { id: "Zagreb", kotinent: "E"},
     { id: "Pariz", kotinent: "E"},
-    { id: "Egipat", kotinent: "Af"},
+    { id: "Kairo", kotinent: "Af"},
+    { id: "Sarajevo", kotinent: "E"},
+    { id: "Berlin", kotinent: "E"},
+    { id: "..", kotinent: "E"},
+    { id: "Pariz", kotinent: "E"},
+    { id: "Pariz", kotinent: "E"},
+    { id: "Pariz", kotinent: "E"},
+    { id: "Pariz", kotinent: "E"},
     { id: "Pariz", kotinent: "E"},
 ];
